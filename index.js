e5686acba8e16bcbe2a217d05e21ac840274c2cd
sebug <sebastian.gfeller@gmail.com>
2017-07-23 00:26:13 +0200

    With adding require entry

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var esprima = require('esprima');
+var es = require('escodegen');
 var fs = require('fs');
 
 function getRequires(program) {
@@ -28,9 +29,93 @@ function getRequires(program) {
     }
 }
 
+function addRequire(tree, r) {
+    var res;
+    if (tree.type === 'Program') {
+	res = {};
+	Object.keys(tree).forEach(function (k) {
+	    if (k === 'body') {
+		res[k] = tree[k].map(function (b) {
+		    return addRequire(b, r);
+		});
+	    } else {
+		res[k] = tree[k];
+	    }
+	});
+	return res;
+    } else if (tree.type === 'ExpressionStatement') {
+	res = {};
+	Object.keys(tree).forEach(function (k) {
+	    if (k === 'expression') {
+		res[k] = addRequire(tree[k], r);
+	    } else {
+		res[k] = tree[k];
+	    }
+	});
+	return res;
+    } else if (tree.type === 'CallExpression') {
+	res = {};
+	Object.keys(tree).forEach(function (k) {
+	    if (k === 'arguments') {
+		res[k] = tree[k].map(function (arg) {
+		    return addRequire(arg, r);
+		});
+	    } else {
+		res[k] = tree[k];
+	    }
+	});
+	return res;
+    } else if (tree.type === 'ArrayExpression') {
+	// Found the array expression
+		res = {};
+	Object.keys(tree).forEach(function (k) {
+	    if (k === 'elements') {
+		res[k] = insertRequire(tree[k], r);
+	    } else {
+		res[k] = tree[k];
+	    }
+	});
+	return res;
+    } else {
+	return tree;
+    }
+}
+
+function insertRequire(elements, r) {
+    var result = [];
+    var i;
+    for (i = 0; i < elements.length && elements[i].value < r; i += 1) {
+	result.push(elements[i]);
+    }
+    result.push({
+	type: 'Literal',
+	value: r,
+	raw: '\'' + r + '\''
+    });
+    for (;i < elements.length; i += 1) {
+	result.push(elements[i]);
+    }
+    return result;
+}
+
+var search = 'app/B';
+
 fs.readFile('data/test.js', 'utf8', function (err, data) {
     var tree = esprima.parseScript(data, { range: true, tokens: true, comment: true });
-    console.log(tree);
+    var enriched = es.attachComments(tree, tree.comments, tree.tokens);
+    
     var requires = getRequires(tree);
-    console.log(requires);
+
+    var found = requires.filter(function (item) {
+	return item.value === search;
+    }).length;
+    if (found) {
+	console.log(data);
+    } else {
+	var transformed = addRequire(
+	    tree,
+	    search
+	);
+	console.log(es.generate(transformed, { comment: true }));
+    }
 });
